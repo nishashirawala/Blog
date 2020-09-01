3bbe206f579b6b6e17a1de6e6fb79e3ffaaa4e8d
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-09-01 08:10:26 -0400

    Adding axios interceptors  (Section 9 #171)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(requestConfig => {
+    console.log(requestConfig);
+    // Edit the config here, may be headers, token..etc.
+    return requestConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use (response => {
+    console.log(response);
+    // Edit the config here, may be headers, token..etc.
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
