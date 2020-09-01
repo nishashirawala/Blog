770884abc7f8da7ba40e8c5ef28b36e7bf2cc8f0
Nisha Shirawala <nisha.shirawala@intelliware.com>
2020-09-01 08:15:01 -0400

    Default global config on axios (Section 9 #173)

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,7 +10,7 @@ class FullPost extends Component {
     componentDidUpdate(prevProps, prevState, snapshot) {
         if(this.props.id) {
             if(!this.state.loadedPost ||  (this.state.loadedPost && this.props.id !== this.state.loadedPost.id)) {
-                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.id)
                     .then(response => {
                         console.log(response);
                         this.setState({loadedPost: response.data})
@@ -20,7 +20,7 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () => {
-        axios.delete('https://jsonplaceholder.typicode.com/posts/'+this.props.id).then(response => {
+        axios.delete('/posts/'+this.props.id).then(response => {
             console.log('delete success', response);
         })
     }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,7 +14,7 @@ class Blog extends Component {
         error: false
     };
     componentDidMount() {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
+        axios.get("/posts")
             .then(response => {
                 console.log(response);
                 const posts = response.data.slice(0,4);
